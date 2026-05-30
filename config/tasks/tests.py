033a961db2d83d17e741fde30638d522a4e87ede
carsearin <thuleanperspectif@gmail.com>
2026-05-30 20:24:27 +0200

    TRACK-2 Added test for Task model creation and split it with database connection task into different classes

--- config/tasks/tests.py
+++ config/tasks/tests.py
@@ -1,5 +1,11 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
 from django.db import connection
+from django.test import TestCase
+
+from tasks.models import Task
+
+
+User = get_user_model()
 
 
 class DatabaseConnectionTest(TestCase):
@@ -9,3 +15,21 @@ class DatabaseConnectionTest(TestCase):
             result = cursor.fetchone()
 
         self.assertEqual(result[0], 1)
+
+
+class TaskModelTest(TestCase):
+    def test_create_task(self):
+        user = User.objects.create_user(
+            username="test_user",
+            password="test_password"
+        )
+
+        task = Task.objects.create(
+            title="Learn Django",
+            user=user
+        )
+
+        self.assertEqual(task.title, "Learn Django")
+        self.assertEqual(task.status, Task.Status.TODO)
+        self.assertEqual(task.user, user)
+        self.assertIsNotNone(task.created_at)
