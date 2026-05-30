3dce5cbf505cd3df07654214a72dbb7887240994
carsearin <thuleanperspectif@gmail.com>
2026-05-30 13:22:26 +0200

    TRACK-2 Created Task model, admin and applied migrations

--- config/tasks/admin.py
+++ config/tasks/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "status",
+        "user",
+        "created_at",
+    )
