ffa755887b3c95b445814c42a1e52d0a9d00839d
carsearin <thuleanperspectif@gmail.com>
2026-06-04 14:14:49 +0200

    TRACK-3 Added and ran tests including password storage check

--- config/users/tests.py
+++ config/users/tests.py
@@ -1,3 +1,54 @@
+import json
+
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+User = get_user_model()
+
+
+class RegisterViewTest(TestCase):
+
+    def test_register_user_success(self):
+        response = self.client.post(
+            "/auth/register",
+            data=json.dumps({
+                "username": "test_user",
+                "password": "test_password",
+            }),
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(User.objects.count(), 1)
+
+        user = User.objects.get(username="test_user")
+
+        self.assertEqual(user.username, "test_user")
+        self.assertNotEqual(user.password, "test_password")
+        self.assertTrue(user.check_password("test_password"))
+
+    def test_register_user_missing_fields(self):
+        response = self.client.post(
+            "/auth/register",
+            data=json.dumps({}),
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_register_user_duplicate_username(self):
+        User.objects.create_user(
+            username="test_user",
+            password="test_password",
+        )
+
+        response = self.client.post(
+            "/auth/register",
+            data=json.dumps({
+                "username": "test_user",
+                "password": "another_password",
+            }),
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, 400)
